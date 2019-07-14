955ef6b8379d84bdab6c50f4222279aeca3fef40
GowthamiRadha <37111979+GowthamiRadha@users.noreply.github.com>
2019-07-14 08:47:52 +0530

    Forum java file

---
+++ forum.java
@@ -0,0 +1,14 @@
+package com.example.cfg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+    }
+}
